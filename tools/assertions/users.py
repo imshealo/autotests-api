34ab60e954fa11c87715fbd3574dab675076ab30
imshealo <kostukovmihail48@gmail.com>
2025-12-28 20:00:50 +0200

    Фикстуры Pytest. Практика написания автотестов с использованием фикстур

--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -1,4 +1,5 @@
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema, \
+    UserSchema
 from tools.assertions.base import assert_equal
 
 
@@ -11,9 +12,41 @@ def assert_create_user_response(request: CreateUserRequestSchema, response: Crea
         response: Ответ API с данными пользователя.
 
     Raises:
-        AssertionError: Если хотя бы одно поле не совпадает
+        AssertionError: Если хотя бы одно поле не совпадает.
     """
     assert_equal(response.user.email, request.email, "email")
     assert_equal(response.user.last_name, request.last_name, "last_name")
     assert_equal(response.user.first_name, request.first_name, "first_name")
     assert_equal(response.user.middle_name, request.middle_name, "middle_name")
+
+
+def assert_user(actual: UserSchema, expected: UserSchema):
+    """
+    Проверяет корректность данных пользователя.
+
+    Args:
+        actual: Фактические данные пользователя.
+        expected: Ожидаемые данные пользователя.
+
+    Raises:
+        AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.email, expected.email, "email")
+    assert_equal(actual.last_name, expected.last_name, "last_name")
+    assert_equal(actual.first_name, expected.first_name, "first_name")
+    assert_equal(actual.middle_name, expected.middle_name, "middle_name")
+
+
+def assert_get_user_response(get_user_response: GetUserResponseSchema, create_user_response: CreateUserResponseSchema):
+    """
+    Проверяет корректность данных пользователя при получении и создании пользователя.
+
+    Args:
+        get_user_response: Ответ API на запрос пользователя.
+        create_user_response: Ответ API на создание пользователя.
+
+    Raises:
+        AssertionError: Если данные пользователя не совпадают.
+    """
+    assert_user(get_user_response.user, create_user_response.user)
